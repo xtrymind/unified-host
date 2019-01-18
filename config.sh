eacc696b520a8dba47bdfbb094d5e215fabdd247
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-01-18 18:49:03 +0700

    config: change permission for uhost
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- config.sh
+++ config.sh
@@ -86,7 +86,7 @@ set_permissions() {
 
   # The following is default permissions, DO NOT remove
   set_perm_recursive  $MODPATH  0  0  0755  0644
-  set_perm  $MODPATH/system/bin/uh  0  0  0777
+  set_perm  $MODPATH/system/bin/uhost  0  0  0777
 }
 
 ##########################################################################################
